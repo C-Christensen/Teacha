e67615501011b592c224b39ed44079fc26c151a3
Camila Christensen <52684850+C-Christensen@users.noreply.github.com>
2020-08-13 01:00:02 -0700

    Add files via upload

---
+++ src/data/dashboard-circles.js
@@ -0,0 +1,41 @@
+const DashboardCircles = [
+    {
+        "id": 1,
+        "title": "Seattle Public Schools",
+        "members": 120,
+        "image": require("../assets/s-public-schools.png")
+    },
+
+    {
+        "id": 2,
+        "title": "Denver School District",
+        "members": 140,
+        "image": require("../assets/denver.jpg")
+    },
+    {
+        "id": 3,
+        "title": "Albuquerque Public Schools",
+        "members": 550,
+        "image": require("../assets/aps.png")
+    },
+    {
+        "id": 4,
+        "title": "Orange County Public Schools",
+        "members": 70,
+        "image": require("../assets/orangecounty.png")
+    },
+    {
+        "id": 5, 
+        "title": "Volcano Vista",
+        "members": 30,
+        "image": require("../assets/volcanoVista.png")
+    },
+    {
+        "id": 6, 
+        "title": "Clevaland High School",
+        "members": 20,
+        "image": require("../assets/clevelandHigh.png")
+    }
+];
+
+export default DashboardCircles;
---
+++ src/data/new-circles-json.js
@@ -0,0 +1,60 @@
+const NewCircles = [
+    {
+        "id": 1,
+        "title": "High School Teachers",
+        "size": 350,
+        "image": require("../assets/black_teacher.png")
+    },
+
+    {
+        "id": 2,
+        "title": "Teachers of Color Science Club",
+        "size": 230,
+        "image": require("../assets/science-poc.png")
+    },
+    {
+        "id": 3,
+        "title": "Hispanic Teacher Association",
+        "size": 350,
+        "image": require("../assets/hispanic-teachers.png")
+    },
+    {
+        "id": 4, 
+        "title": "Teachers of Seattle School District",
+        "size": 250,
+        "image": require("../assets/seattle-excellence.png")
+        
+    },
+    {
+        "id": 5, 
+        "title": "First Grade Teachers",
+        "size": 500,
+        "image": require("../assets/first-grade-teacher.png")
+    },
+    {
+        "id": 6, 
+        "title": "Seattle Public Schools",
+        "size": 150,
+        "image": require("../assets/s-public-schools.png")
+    },
+    {
+        "id": 7, 
+        "title": "Seattle Creative High School English",
+        "size": 250,
+        "image": require("../assets/seattle-creative-school.png")
+    },
+    {
+        "id": 8, 
+        "title": "Seattle Teachers Advocacy Group",
+        "size": 250,
+        "image": require("../assets/advocacy.png")
+    },
+    {
+        "id": 9,
+        "title": "Seattle School District",
+        "size": 250,
+        "image": require("../assets/seattle-district.png")
+    }
+];
+
+export default NewCircles;
